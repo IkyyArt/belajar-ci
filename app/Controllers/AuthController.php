5bc143733139abaf2a39aace33f29e128f681142
Rizky Delfa <rizkydelfa05@gmail.com>
2026-05-13 08:31:35 +0700

    Capstone 6: Migration & Seeding

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -18,7 +18,7 @@ public function login()
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = ['username' => 'rizky', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin', 'email' => 'a1112415687@mhs.dinus.ac.id']; // passw 123
+        $dataUser = ['username' => 'rizky', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin', 'email' => 'a1112415687@mhs.dinus.ac.id']; 
 
         if ($username == $dataUser['username']) {
             if (md5($password) == $dataUser['password']) {
