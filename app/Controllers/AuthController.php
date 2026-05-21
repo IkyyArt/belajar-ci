ebb9028f51a1d2b039bd720319800b6d66e7a48c
Rizky Delfa <rizkydelfa05@gmail.com>
2026-05-21 13:29:54 +0700

    capstone-7: read data validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,23 +5,33 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
+    protected $userModel;
     function __construct()
 {
     helper('form');
+    $this->userModel = new UserModel();
 }
 
 public function login()
 {
     if ($this->request->getPost()) {
+        $rules = [
+    'username' => 'required|min_length[6]',
+    'password' => 'required|min_length[7]|numeric',
+        ];
+        
+        if ($this->validate($rules)) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = ['username' => 'rizky', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin', 'email' => 'a1112415687@mhs.dinus.ac.id']; 
+        $dataUser = $this->userModel ->where(['username' => $username])->first();
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
+        if ($dataUser) {
+	        if (password_verify($password, $dataUser['password'])) {
                 session()->set([
                     'username'   => $dataUser['username'],
                     'role'       => $dataUser['role'],
@@ -39,6 +49,10 @@ public function login()
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
             return redirect()->back();
         }
+        } else {
+    session()->setFlashdata('failed', $this->validator->listErrors());
+    return redirect()->back();
+}
     } else {
         return view('v_login');
     }
