67c8d97b31da53a5ce757fb5e6ddec5aaeca6668
Rizky Delfa <rizkydelfa05@gmail.com>
2026-06-02 10:22:15 +0700

    Add profile route and link

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -24,3 +24,6 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
 // Keranjang
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 
+// Profile
+$routes->get('profile', 'ProfileController::index', ['filter' => 'auth']);
+
